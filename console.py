b463b426d9fd5f614ab1645caf9c8bd75e3fd5bc
Nahom Fikadu <nahi.terefe@gmail.com>
2022-02-01 00:50:36 +0300

    Update console.py

--- console.py
+++ console.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
 """
-    console module    
+   AirBnB console module    
 """
 
 import cmd
+import sys
+import json
+import os
 from models import storage
 from models.base_model import BaseModel
-
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 class HBNBCommand(cmd.Cmd):
     """ HBNB Command class """
